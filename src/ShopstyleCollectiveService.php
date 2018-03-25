610dde825d529a634691e1dd734005d461b03633
Jarret Minkler <jarret@domyownpestcontrol.com>
2018-03-24 23:46:48 -0400

    Category options, refactoring

--- src/ShopstyleCollectiveService.php
+++ src/ShopstyleCollectiveService.php
@@ -9,33 +9,91 @@
 namespace Jminkler\LaravelShopstyleCollective;
 
 use GuzzleHttp\Client;
+use Log;
 
 class ShopstyleCollectiveService
 {
     protected $apiKey;
     protected $base_url;
+    protected $options;
 
     public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
+        $this->base_uri = config('shopstyle.base_uri');
         $this->client = new Client([
-            'base_uri' => config('shopstyle.base_uri'),
+            'base_uri' => $this->base_uri,
             'timeout' => 2.0,
             'debug' => true,
         ]);
-
+        $this->options = ['pid' => $this->apiKey];
     }
 
-    public function categories()
+    public function categories($cat = null, $depth = null)
     {
-        $url = config('shopstyle.base_uri', 'http://api.shopstyle.com/api/v2') . '/categories';
+        $action = 'categories';
+        $options = $this->getCategoriesOptions($cat, $depth);
 
-        return $this->client->request(
-            'GET',
-            $url,
-            ['query' => 'pid=' . $this->apiKey]
-        );
-
+        return $this->getJsonResponse($action, $options);
     }
+
+    private function getJson($response)
+    {
+        $json = json_decode($response->getBody()->getContents());
+        return $json;
+    }
+
+    /**
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    private function get($action, $options)
+    {
+        $options['query'] = http_build_query(array_merge($this->options, $options));
+        Log::info(__CLASS__, [$options]);
+        $response = $this->client->get(
+            $action,
+            $options
+        );
+        return $response;
+    }
+
+    /**
+     * @param $cat
+     * @param $depth
+     * @return array
+     */
+    private function getCategoriesOptions($cat, $depth)
+    {
+        $options = [];
+        if ($cat) {
+            $options['cat'] = $cat;
+        }
+        if ($depth) {
+            $options['depth'] = $depth;
+        }
+        return $options;
+    }
+
+    /**
+     * @param $action
+     * @param $options
+     * @return null
+     */
+    private function getJsonResponse($action, $options)
+    {
+        $response = $this->get($action, $options);
+
+        if ($response->getBody()) {
+            $json = $this->getJson($response);
+            if (count($json->$action) > 0) {
+                return $json->$action;
+            }
+
+            return $json->metadata->root;
+        }
+
+        return null;
+    }
+
 
 }
--- tests/ShopstyleCollectiveServiceTest.php
+++ tests/ShopstyleCollectiveServiceTest.php
@@ -14,8 +14,27 @@ class ShopstyleCollectiveServiceTest extends TestCase
         $obj = new ShopstyleCollectiveService(config('shopstyle.api_key'));
 
         $categories = $obj->categories();
-
         $this->assertTrue(count($categories) > 0, 'We have categories');
-
     }
+
+    /** @test */
+    public function can_list_categories_depth_1()
+    {
+        $obj = new ShopstyleCollectiveService(config('shopstyle.api_key'));
+
+        $categories = $obj->categories(null, 1);
+        $this->assertTrue(count($categories) > 0, 'We have categories');
+    }
+
+    /** @test */
+    public function can_list_categories_under_womens()
+    {
+        $obj = new ShopstyleCollectiveService(config('shopstyle.api_key'));
+
+        $categories = $obj->categories(214);
+
+        $this->assertEquals('mens-dress-shirts', $categories->id, 'We have mens-dress-shirts');
+    }
+
+
 }
